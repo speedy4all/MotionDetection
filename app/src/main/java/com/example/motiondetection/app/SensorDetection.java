8f2ee94ed6116328e54a9063db23c1d58a379eb5
speedy4all <speedy4all@gmail.com>
2015-04-01 01:33:22 +0300

    Service up and running ;)

--- app/src/main/java/com/example/motiondetection/app/SensorDetection.java
+++ app/src/main/java/com/example/motiondetection/app/SensorDetection.java
@@ -5,32 +5,32 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.util.Log;
 
-/**
- * Created by Bogdan on 24/03/2015.
- */
+
 public class SensorDetection implements SensorEventListener {
     public static String NORMAL_STATE = "NORMAL";
     public static String SITTING_STATE = "SITTING";
     public static String WALKING_STATE = "WALKING";
     public static String FALLING_STATE = "FALLING";
     public static String STANDING_STATE = "STANDING";
-    public static int FINAL_STATE = 0;
-    public static String FINAL_PREVIOUS_STATE = "";
-    public double ax,ay,az;
-    public double accelerationVector;
+    public static int FINAL_STATE = 4;
+    public static int FALLING_THRESHOLD = 18;
+    public static boolean FALLING = false;
+    public static String CURRENT_STATE = "none", PREVIEW_STATE = "none";
     static int BUFF_SIZE = 50;
     static public double[] sampleData = new double[BUFF_SIZE];
+    public double ax,ay,az;
+    public double accelerationVector;
     double sigmaFilter=0.5,thresholdHigh=10,thresholdLow=5,thresholdMiddle=2;
     private SensorManager sensorManager;
-    public static String CURRENT_STATE = "none",PREVIEW_STATE = "none";
 
     public SensorDetection(Context context){
         for(int i=0; i<BUFF_SIZE; i++){
             sampleData[i]=0;
         }
         sensorManager=(SensorManager) context.getSystemService(context.SENSOR_SERVICE);
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_UI);
 
     }
     @Override
@@ -41,6 +41,7 @@ public class SensorDetection implements SensorEventListener {
             az=event.values[2];
             AddData(ax,ay,az);
             ActivityRecognition(sampleData,ay);
+            DetectFall();
             SystemState(CURRENT_STATE,PREVIEW_STATE);
             if(!PREVIEW_STATE.equalsIgnoreCase(CURRENT_STATE)){
                 PREVIEW_STATE  = CURRENT_STATE;
@@ -54,21 +55,21 @@ public class SensorDetection implements SensorEventListener {
 
         //Fall !!
         if(!prev_state.equalsIgnoreCase(curr_state)){
-            if(curr_state.equalsIgnoreCase(FALLING_STATE)){
+            if (FALLING && (CURRENT_STATE.equalsIgnoreCase(SITTING_STATE) || CURRENT_STATE.equalsIgnoreCase(NORMAL_STATE))) {
                 FINAL_STATE = 0;
-                //
+                Log.e(FALLING_STATE, FALLING_STATE);
             }
             if(curr_state.equalsIgnoreCase(SITTING_STATE)){
                FINAL_STATE = 1;
-               //
+                Log.e(SITTING_STATE, SITTING_STATE);
             }
             if(curr_state.equalsIgnoreCase(STANDING_STATE)){
                 FINAL_STATE = 2;
-                //
+                Log.e(STANDING_STATE, STANDING_STATE);
             }
             if(curr_state.equalsIgnoreCase(WALKING_STATE)){
+                Log.e(WALKING_STATE, WALKING_STATE);
                 FINAL_STATE = 3;
-                //
             }
         }
 
@@ -86,39 +87,51 @@ public class SensorDetection implements SensorEventListener {
             if( (samplingData[i]-thresholdHigh) < sigmaFilter && (samplingData[i-1]-thresholdHigh) > sigmaFilter)
                 count++;
         }
+        //Log.i("ZeroCrossingRate", "Val: "+count);
         return count;
     }
     private void AddData(double ax2, double ay2, double az2) {
 
-        accelerationVector = Math.sqrt( ax*ax + ay*ay + az*az );
+        accelerationVector = Math.sqrt(ax2 * ax2 + ay2 * ay2 + az2 * az2);
         for(int i=0 ;i<=BUFF_SIZE-2 ;i++){
             sampleData[i] = sampleData[i+1];
         }
         sampleData[BUFF_SIZE-1] = accelerationVector;
+        //Log.i("Acceleration", "Value: "+accelerationVector);
+
+    }
+
+    private void DetectFall() {
+        int min = Integer.MAX_VALUE, max = 0;
+        for (int i = 1; i < sampleData.length; i++) {
+            if (sampleData[i] - sampleData[i - 1] > max) {
+                max = (int) sampleData[i] - (int) sampleData[i - 1];
+            }
+            if (sampleData[i] - sampleData[i - 1] < min) {
+                min = (int) sampleData[i] - (int) sampleData[i - 1];
+            }
+        }
+        if (max - min > FALLING_THRESHOLD) {
+            FALLING = true;
+        }
+        Log.e("DetectFall", "Value min-max: " + (max - min));
 
     }
     private void ActivityRecognition(double[] sample,double ay2) {
 
         int zeroCrossingRate = ComputeZeroCrossingRate(sample);
         if(zeroCrossingRate == 0){
-
-            if( Math.abs(ay2) < thresholdHigh){
+            // Log.e("AY2", "Val: "+ Math.abs(ay2));
+            if (Math.abs(ay2) < thresholdLow) {
                 CURRENT_STATE = SITTING_STATE;
             }else{
                 CURRENT_STATE = STANDING_STATE;
             }
 
-        }else{
-
-            if(zeroCrossingRate > thresholdMiddle ){
+        } else if (zeroCrossingRate > thresholdMiddle) {
                 CURRENT_STATE = WALKING_STATE;
             }else{
                 CURRENT_STATE = NORMAL_STATE;
             }
-
-        }
-
-
-
     }
 }
--- app/src/main/java/com/example/motiondetection/app/WorkService.java
+++ app/src/main/java/com/example/motiondetection/app/WorkService.java
@@ -1,6 +1,5 @@
 package com.example.motiondetection.app;
 
-import android.app.ActivityManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -9,77 +8,107 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.SoundPool;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
-import java.util.HashMap;
-
-/**
- * Created by Bogdan on 24/03/2015.
- */
-
 
     public class WorkService extends Service {
 
-    private boolean isRunning  = false;
-    private NotificationManager notificationManager;
-    private int NOTIFICATION = 1;
-
-    private static SoundPool soundPool;
-    private static HashMap soundPoolMap;
 
     public static int CURRENT_STATE = 0;
     public static String SITTING_STATE = "SITTING";
     public static String WALKING_STATE = "WALKING";
     public static String FALLING_STATE = "FALLING";
     public static String STANDING_STATE = "STANDING";
-
     public static int FALLING;
     public static int SITTING ;
     public static int STANDING;
     public static int WALKING ;
-    private Context context;
+        private static boolean RUNNING = false;
+        Runnable refresh = new Runnable() {
+            @Override
+            public void run() {
+                RUNNING = true;
+                try {
+                    if (sensorActivity.FINAL_STATE != CURRENT_STATE) {
+                        if (sensorActivity.FINAL_STATE == 0) {
+                            CURRENT_STATE = 0;
+                            showActivityNotification(FALLING_STATE);
+                            soundPool.play(FALLING, 1f, 1f, 1, 0, 1f);
+                            sensorActivity.FALLING = false;
+                        } else if (sensorActivity.FINAL_STATE == 3) {
+                            CURRENT_STATE = 3;
+                            showActivityNotification(WALKING_STATE);
+                            soundPool.play(WALKING, 1f, 1f, 1, 0, 1f);
+                        } else if (sensorActivity.FINAL_STATE == 1) {
+                            CURRENT_STATE = 1;
+                            showActivityNotification(SITTING_STATE);
+                            soundPool.play(SITTING, 1f, 1f, 1, 0, 1f);
+                        } else if (sensorActivity.FINAL_STATE == 2) {
+                            CURRENT_STATE = 2;
+                            showActivityNotification(STANDING_STATE);
+                            soundPool.play(STANDING, 1f, 1f, 1, 0, 1f);
+                        }
+                    }
 
+                    Log.d(getString(R.string.app_name), "Service running");
+
+                } catch (Exception e) {
+                }
+
+                mHandler.postDelayed(this, 2500);
+            }
+        };
+        private static SoundPool soundPool;
+        private final Handler mHandler = new Handler();
+        private NotificationManager notificationManager;
+        private int NOTIFICATION = 1;
     private SensorDetection sensorActivity;
-    @Override
-    public void onCreate() {
-        notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-        sensorActivity = new SensorDetection(this);
-        soundPool = new SoundPool(2, AudioManager.STREAM_MUSIC, 100);
-        soundPoolMap = new HashMap();
-        context = this;
-        FALLING =  soundPool.load(this, R.raw.falling, 1);
-        SITTING = soundPool.load(this, R.raw.sitting, 2);
-        STANDING = soundPool.load(this, R.raw.standing, 3);
-        WALKING = soundPool.load(this, R.raw.walking, 4);
 
-        // Display a notification about us starting.  We put an icon in the status bar.
-        showNotification();
-    }
-    private void showActivityNotification(String activity) {
-        // In this sample, we'll use the same text for the ticker and the expanded notification
-        CharSequence text = "We detected that you are: " + activity;
-
-        // Set the icon, scrolling text and timestamp
-        Notification notification = new Notification(R.drawable.ic_launcher, text,
-                System.currentTimeMillis());
-
-        // The PendingIntent to launch our activity if the user selects this notification
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-                new Intent(this, HomeActivity.class), 0);
-
-        // Set the info for the views that show in the notification panel.
-        notification.setLatestEventInfo(this, getText(R.string.app_name),
-                text, contentIntent);
-        notification.flags = Notification.DEFAULT_LIGHTS | Notification.FLAG_AUTO_CANCEL ;
-        // Send the notification.
-        notificationManager.notify(NOTIFICATION, notification);
+        public static boolean isServiceRunning(Context context, Class<?> serviceClass) {
+            return RUNNING;
     }
 
-    private void showNotification() {
+        @Override
+        public void onCreate() {
+            notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            sensorActivity = new SensorDetection(this);
+            soundPool = new SoundPool(2, AudioManager.STREAM_MUSIC, 100);
+
+            FALLING = soundPool.load(this, R.raw.falling, 1);
+            SITTING = soundPool.load(this, R.raw.sitting, 2);
+            STANDING = soundPool.load(this, R.raw.standing, 3);
+            WALKING = soundPool.load(this, R.raw.walking, 4);
+
+            // Display a notification about us starting.  We put an icon in the status bar.
+            showNotification();
+    }
+
+        private void showActivityNotification(String activity) {
         // In this sample, we'll use the same text for the ticker and the expanded notification
-        CharSequence text = getText(R.string.app_name)+ " Service started";
+            CharSequence text = "We detected that you are: " + activity;
+
+            // Set the icon, scrolling text and timestamp
+            Notification notification = new Notification(R.drawable.ic_launcher, text,
+                    System.currentTimeMillis());
+
+            // The PendingIntent to launch our activity if the user selects this notification
+            PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                    new Intent(this, HomeActivity.class), 0);
+
+            // Set the info for the views that show in the notification panel.
+            notification.setLatestEventInfo(this, getText(R.string.app_name),
+                    text, contentIntent);
+            notification.flags = Notification.DEFAULT_LIGHTS | Notification.FLAG_AUTO_CANCEL;
+            // Send the notification.
+            notificationManager.notify(NOTIFICATION, notification);
+        }
+
+        private void showNotification() {
+            // In this sample, we'll use the same text for the ticker and the expanded notification
+            CharSequence text = getText(R.string.app_name) + " Service started";
 
         // Set the icon, scrolling text and timestamp
         Notification notification = new Notification(R.drawable.ic_launcher, text,
@@ -96,7 +125,8 @@ import java.util.HashMap;
         // Send the notification.
         notificationManager.notify(NOTIFICATION, notification);
     }
-    @Override
+
+        @Override
         public IBinder onBind(Intent arg0) {
             return null;
         }
@@ -104,67 +134,20 @@ import java.util.HashMap;
         @Override
         public int onStartCommand(Intent intent, int flags, int startId) {
             // Let it continue running until it is stopped.
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-
-
-                    //Your logic that service will perform will be placed here
-                    //In this example we are just looping and waits for 1000 milliseconds in each loop.
-                    while(true) {
-                        try {
-                           if(sensorActivity.FINAL_STATE != CURRENT_STATE) {
-                               if (sensorActivity.FINAL_STATE == 0) {
-                                   CURRENT_STATE = 0;
-                                   showActivityNotification(FALLING_STATE);
-                                   soundPool.play(FALLING, 1f, 1f, 1, 0, 1f);
-                               } else if (sensorActivity.FINAL_STATE == 3) {
-                                   CURRENT_STATE = 3;
-                                   showActivityNotification(WALKING_STATE);
-                                   soundPool.play(WALKING, 1f, 1f, 1, 0, 1f);
-                               } else if (sensorActivity.FINAL_STATE == 1) {
-                                   CURRENT_STATE = 1;
-                                   showActivityNotification(SITTING_STATE);
-                                   soundPool.play(SITTING, 1f, 1f, 1, 0, 1f);
-                               } else if (sensorActivity.FINAL_STATE == 2) {
-                                   CURRENT_STATE = 2;
-                                   showActivityNotification(STANDING_STATE);
-                                   soundPool.play(STANDING, 1f, 1f, 1, 0, 1f);
-                               }
-                           }
-                            Log.d(getString(R.string.app_name), "Service running");
-                            Thread.sleep(2000);
-                        } catch (Exception e) {
-                        }
-
-
-                    }
-
-
-                }
-            }).start();
-
-
-            Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+            mHandler.postDelayed(refresh, 500);
+            Toast.makeText(getApplicationContext(), "Service Started", Toast.LENGTH_LONG).show();
             return START_STICKY;
         }
+
         @Override
         public void onDestroy() {
+            RUNNING = false;
+            mHandler.removeCallbacks(refresh);
             sensorActivity.Stop();
             sensorActivity = null;
             Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
             super.onDestroy();
         }
-
-    public static boolean isServiceRunning(Context context, Class<?> serviceClass) {
-        ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-            if (service.service.getClassName().equals(serviceClass.getName())) {
-                return true;
-            }
-        }
-        return false;
-    }
 
 }
 
